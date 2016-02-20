25b1c140c608e7e2ebb0a572f6da7a8f216dcf37
Ivan Gromakovsky <gie1994@gmail.com>
2016-02-20 18:42:00 +0300

    Add Serokell.Util

---
+++ src/Serokell/Util.hs
@@ -0,0 +1,14 @@
+-- | Re-export everything from Serokell.Util.*
+
+module Serokell.Util
+       (
+         module Serokell.Util.Exceptions
+       , module Serokell.Util.Lens
+       , module Serokell.Util.Text
+       , module Serokell.Util.Verify
+       ) where
+
+import           Serokell.Util.Exceptions
+import           Serokell.Util.Lens
+import           Serokell.Util.Text
+import           Serokell.Util.Verify
