c08f96d13da19aab2ce4ff153d3ca5fbbfea5869
Ivan Gromakovsky <gie1994@gmail.com>
2016-03-27 18:22:04 +0300

    Add OptParse module

--- src/Serokell/Util.hs
+++ src/Serokell/Util.hs
@@ -8,6 +8,7 @@ module Serokell.Util
 import           Serokell.Util.AcidState    as Exports
 import           Serokell.Util.Exceptions   as Exports
 import           Serokell.Util.Lens         as Exports
+import           Serokell.Util.OptParse     as Exports
 import           Serokell.Util.StaticAssert as Exports
 import           Serokell.Util.Text         as Exports
 import           Serokell.Util.Verify       as Exports
