243c4e952baf0502a466f885936a1aac3ed09e47
Arseniy Seroka <ars.seroka@gmail.com>
2016-05-02 19:26:43 +0300

    nixify

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, acid-state, base, exceptions, lens, mtl
+      , optparse-applicative, stdenv, template-haskell, text, text-format
+      , transformers
+      }:
+      mkDerivation {
+        pname = "serokell-core";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          acid-state base exceptions lens mtl optparse-applicative
+          template-haskell text text-format transformers
+        ];
+        homepage = "http://gitlab.serokell.io/serokell-team/serokell-core";
+        description = "General-purpose functions by Serokell";
+        license = stdenv.lib.licenses.unfree;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
