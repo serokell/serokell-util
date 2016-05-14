92e28f366bcf3692df6c7c0f291cd8fc16958aa4
Ivan Gromakovsky <gie1994@gmail.com>
2016-05-14 19:39:07 +0300

    Add indexModulo functions

--- src/Serokell/Util/Common.hs
+++ src/Serokell/Util/Common.hs
@@ -2,9 +2,13 @@
 
 module Serokell.Util.Common
        ( enumerate
+       , indexModulo
+       , indexModuloMay
        ) where
 
 import           Control.Monad.State (evalState, get, modify)
+import           Data.List           (genericIndex, genericLength)
+import           Data.Maybe          (fromMaybe)
 
 -- | Enumerate function is analogous to python's enumerate. It
 -- takes sequences of values and returns sequence of pairs where the
@@ -21,3 +25,23 @@ enumerate values = evalState action 0
         i <- get
         modify succ
         return (i, v)
+
+-- | Returns element of a list with given index modulo length of
+-- list. Raises error if list is empty.
+-- Examples:
+-- indexModulo [1, 2, 3] 10 = 2
+-- indexModulo [1, 0] 2 = 1
+-- indexModulo [] 199 = error
+indexModulo :: Integral i => [a] -> i -> a
+indexModulo xs =
+    fromMaybe (error "Serokell.Util.Common.indexModulo: empty list") .
+    indexModuloMay xs
+
+-- | Behaves like `indexModulo` but uses Maybe to report error
+-- (i. e. empty list).
+indexModuloMay :: Integral i => [a] -> i -> Maybe a
+indexModuloMay xs i = genericIndex xs <$> indexModuloIndex xs i
+
+indexModuloIndex :: Integral i => [a] -> i -> Maybe i
+indexModuloIndex [] _ = Nothing
+indexModuloIndex xs i = Just $ i `mod` genericLength xs
