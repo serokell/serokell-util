95f8e72c18fde71882235aec9f9b6d6626cb0cd2
Ivan Gromakovsky <gie1994@gmail.com>
2016-06-18 20:13:47 +0300

    Add getWallTime and getCpuTime to Bench module

--- src/Serokell/Util/Bench.hs
+++ src/Serokell/Util/Bench.hs
@@ -1,16 +1,31 @@
 -- | Benchmark related utils.
 
 module Serokell.Util.Bench
-       ( ElapsedTime (..)
+       ( getWallTime
+       , getCpuTime
+       , ElapsedTime (..)
        , measureTime
        , measureTime_
        , perSecond
        ) where
 
 import           Data.Text.Buildable (Buildable (build))
+import           Data.Time.Units     (Nanosecond, TimeUnit, convertUnit)
 import           Formatting          (bprint, shown, (%))
 import           System.Clock        (Clock (..), TimeSpec, diffTimeSpec,
                                       getTime, toNanoSecs)
+
+getWallTime :: TimeUnit a => IO a
+getWallTime = timeSpecToUnit <$> getTime Realtime
+
+getCpuTime :: TimeUnit a => IO a
+getCpuTime = timeSpecToUnit <$> getTime ProcessCPUTime
+
+timeSpecToUnit
+    :: TimeUnit a
+    => TimeSpec -> a
+timeSpecToUnit =
+    convertUnit . (fromIntegral :: Integer -> Nanosecond) . toNanoSecs
 
 data ElapsedTime = ElapsedTime
     { elapsedCpuTime  :: TimeSpec
