9a00d9ba49b55b344d9840c79048ba0b35f5f311
Ivan Gromakovsky <gie1994@gmail.com>
2016-07-15 19:42:46 +0300

    Add JsonByteString

--- src/Serokell/Util/Base64.hs
+++ src/Serokell/Util/Base64.hs
@@ -3,8 +3,11 @@
 module Serokell.Util.Base64
        ( encode
        , decode
+       , JsonByteString (..)
        ) where
 
+import           Control.Monad           ((>=>))
+import           Data.Aeson              (FromJSON (parseJSON), ToJSON (toJSON))
 import qualified Data.ByteString         as BS
 import qualified Data.ByteString.Base64  as B64
 import           Data.Either.Combinators (mapLeft)
@@ -18,3 +21,16 @@ encode = decodeUtf8 . B64.encode
 -- | Decode base64-encoded ByteString.
 decode :: T.Text -> Either T.Text BS.ByteString
 decode = mapLeft T.pack . B64.decode . encodeUtf8
+
+-- | Wrapper on top of ByteString with JSON serialization (in base64
+-- encoding).
+newtype JsonByteString = JsonByteString
+    { getJsonByteString :: BS.ByteString
+    }
+
+instance ToJSON JsonByteString where
+    toJSON = toJSON . encode . getJsonByteString
+
+instance FromJSON JsonByteString where
+    parseJSON =
+        parseJSON >=> either (fail . T.unpack) (pure . JsonByteString) . decode
