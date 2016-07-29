4093720681185aa8ca84911bf95da65917110b15
Dmitry Kovanikov <kovanikov@gmail.com>
2016-07-29 15:05:26 +0300

    [RSC-100] Replace encoding and decoding with safe url versions

--- src/Serokell/Util/Base64.hs
+++ src/Serokell/Util/Base64.hs
@@ -6,13 +6,13 @@ module Serokell.Util.Base64
        , JsonByteString (..)
        ) where
 
-import           Control.Monad           ((>=>))
-import           Data.Aeson              (FromJSON (parseJSON), ToJSON (toJSON))
-import qualified Data.ByteString         as BS
-import qualified Data.ByteString.Base64  as B64
-import           Data.Either.Combinators (mapLeft)
-import qualified Data.Text               as T
-import           Data.Text.Encoding      (decodeUtf8, encodeUtf8)
+import           Control.Monad              ((>=>))
+import           Data.Aeson                 (FromJSON (parseJSON), ToJSON (toJSON))
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Base64.URL as B64
+import           Data.Either.Combinators    (mapLeft)
+import qualified Data.Text                  as T
+import           Data.Text.Encoding         (decodeUtf8, encodeUtf8)
 
 -- | Apply base64 encoding to strict ByteString.
 encode :: BS.ByteString -> T.Text
