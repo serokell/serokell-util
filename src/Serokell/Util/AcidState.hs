b6a45a9ea590700047802154526387d7e58cfdc4
Ivan Gromakovskii <gie1994@gmail.com>
2016-08-18 14:17:16 +0300

    Add createAndDiscardArchive to Util.AcidState

--- src/Serokell/Util/AcidState.hs
+++ src/Serokell/Util/AcidState.hs
@@ -1,17 +1,21 @@
 -- | Some useful functions to work with Data.Acid
 
 module Serokell.Util.AcidState
-       ( readerToQuery
-       , stateToUpdate
+       ( createAndDiscardArchive
        , exceptStateToUpdate
        , exceptStateToUpdateGeneric
+       , readerToQuery
+       , stateToUpdate
        ) where
 
 import           Control.Exception          (Exception, throw)
 import           Control.Monad.Reader       (Reader, asks, runReader)
 import           Control.Monad.State        (State, runState, state)
 import           Control.Monad.Trans.Except (ExceptT, runExceptT)
-import           Data.Acid                  (Query, Update)
+import           Data.Acid                  (AcidState, Query, Update,
+                                             createArchive)
+import           System.Directory           (removeDirectoryRecursive)
+import           System.FilePath            ((</>))
 
 readerToQuery :: Reader s a -> Query s a
 readerToQuery = asks . runReader
@@ -32,3 +36,9 @@ exceptStateToUpdateGeneric toException u =
     runState $
     do res <- runExceptT u
        either (throw . toException) return res
+
+-- | Archive unnecessary data (see createArchive docs for details) and
+-- discard it.
+createAndDiscardArchive :: AcidState st -> FilePath -> IO ()
+createAndDiscardArchive st path =
+    createArchive st >> removeDirectoryRecursive (path </> "Archive")
