3dd2d9e8f49b7f6aabe16a166d63e8e815cc1fe8
Ivan Gromakovskii <gie1994@gmail.com>
2016-08-22 22:18:08 +0300

    Add memory units module

---
+++ src/Serokell/Data/Memory/Units.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE Rank2Types          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns        #-}
+
+-- | Memory units.
+
+module Serokell.Data.Memory.Units
+       (
+         -- | Type class
+         MemoryUnit (..)
+
+         -- | Concrete types
+       , Byte
+       , Kilobyte
+
+         -- | Pretty printing
+       , unitBuilder
+       , memory
+
+         -- | Helpers
+       , toBytes
+       , fromBytes
+       , convertUnit
+       ) where
+
+import           Data.Proxy             (Proxy (Proxy))
+import           Data.Text.Lazy.Builder (Builder)
+import           Data.Typeable          (Typeable)
+import           Formatting             (bprint, stext, (%))
+import qualified Formatting             as Fmt
+
+import           Serokell.Util.Text     (showFixedPretty')
+
+class Integral unit => MemoryUnit unit  where
+    -- | This value is n iff (1 :: unit) is n bytes.
+    bytesMultiplier :: Proxy unit -> Integer
+
+-- | Convert given memory unit into integer representing bytes.
+toBytes
+    :: forall unit.
+       MemoryUnit unit
+    => unit -> Integer
+toBytes mu = toInteger mu * bytesMultiplier proxy
+  where
+    proxy :: Proxy unit
+    proxy = Proxy
+
+-- | Convert given number of bytes into memory unit, flooring value if
+-- necessary.
+fromBytes
+    :: forall unit.
+       MemoryUnit unit
+    => Integer -> unit
+fromBytes bytes = fromInteger $ bytes `div` bytesMultiplier proxy
+  where
+    proxy :: Proxy unit
+    proxy = Proxy
+
+-- | Conversion between memory units.
+convertUnit :: (MemoryUnit a, MemoryUnit b) => a -> b
+convertUnit = fromBytes . toBytes
+
+-- | Construct Text Builder.
+unitBuilder :: MemoryUnit unit => unit -> Builder
+unitBuilder n@(toBytes -> bytes)
+  | bytes == 0 = "0"
+  | bytes < 0 = mconcat ["-", unitBuilder n]
+  | otherwise = bprint (stext % " " % stext) (showFixedPretty' 3 value) suffix
+  where
+    suffixes = ["B", "KiB", "MiB", "GiB", "TiB", "Pib", "EiB", "ZiB", "YiB"]
+    bytesDouble :: Double
+    bytesDouble = realToFrac bytes
+    order = min (length suffixes - 1) (floor $ logBase 2 bytesDouble / 10)
+    suffix = suffixes !! order
+    value = bytesDouble / realToFrac ((2 :: Integer) ^ (order * 10))
+
+-- | Formatter for `formatting` library.
+memory :: MemoryUnit unit => Fmt.Format r (unit -> r)
+memory = Fmt.later unitBuilder
+
+pow10 :: Num n => Int -> n
+pow10 = (10 ^)
+
+newtype Byte =
+    Byte Integer
+    deriving (Show,Eq,Num,Typeable,Integral,Real,Enum,Ord)
+
+instance MemoryUnit Byte where
+    bytesMultiplier Proxy = pow10 0
+
+newtype Kilobyte =
+    Kilobyte Integer
+    deriving (Show,Eq,Num,Typeable,Integral,Real,Enum,Ord)
+
+instance MemoryUnit Kilobyte where
+    bytesMultiplier Proxy = pow10 3
+
+-- P. S. Feel free to add more.
