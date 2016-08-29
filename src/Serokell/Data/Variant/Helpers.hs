2a2c64cb0736294ff96f6533c6f2b51d66b75cc5
Ivan Gromakovskii <gie1994@gmail.com>
2016-08-29 21:34:05 +0300

    Add helper for VarMap construction

--- src/Serokell/Data/Variant/Helpers.hs
+++ src/Serokell/Data/Variant/Helpers.hs
@@ -3,9 +3,19 @@
 
 module Serokell.Data.Variant.Helpers
        ( none
+       , varMap
        ) where
+
+import           Data.Foldable                 (Foldable (toList))
+import qualified Data.HashMap.Strict           as HM hiding (HashMap)
 
 import           Serokell.Data.Variant.Variant (Variant (..))
 
+-- | Shorter alias for VarNone.
 none :: Variant
 none = VarNone
+
+-- | Create VarMap from Foldable containing pairs of Variants.
+-- TODO: maybe use better approach like `a := b` to create KeyValuePair.
+varMap :: Foldable t => t (Variant, Variant) -> Variant
+varMap = VarMap . HM.fromList . toList
