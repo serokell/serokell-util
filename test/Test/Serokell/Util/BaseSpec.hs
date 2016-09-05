f387ec90ead814700264ff6c8acf31af7e38464b
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-09-06 00:12:14 +0100

    [SRK-22] Add testing folder

---
+++ test/Test/Serokell/Util/BaseSpec.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving  #-}
+
+module Test.Serokell.Util.BaseSpec
+       ( spec
+       ) where
+
+import           Data.Aeson                (decode, encode)
+import qualified Data.ByteString           as BS
+import           Data.Maybe                (fromJust)
+import qualified Data.Text                 as T
+import           Test.Hspec                (Spec, describe)
+import           Test.Hspec.QuickCheck     (prop)
+import           Test.QuickCheck           (Arbitrary (..), Gen, (===))
+import           Test.QuickCheck.Instances ()
+
+import qualified Serokell.Util.Base16      as C16
+import qualified Serokell.Util.Base64      as C64
+
+instance Arbitrary C64.JsonByteString where
+    arbitrary = C64.JsonByteString <$> (arbitrary :: Gen BS.ByteString)
+
+deriving instance Eq C64.JsonByteString
+deriving instance Show C64.JsonByteString
+
+spec :: Spec
+spec =
+    describe "Base16/Base64" $ do
+        describe "Indentity Properties" $ do
+            prop "Base16" $
+                \(a :: BS.ByteString) -> a === base16Mid a
+            prop "Base64" $
+                \(a :: BS.ByteString) -> a === base64Mid a
+            prop "JSON Base64" $
+                \(a :: C64.JsonByteString) -> a === base64JSONMid a
+
+base16Mid,
+    base64Mid :: BS.ByteString -> BS.ByteString
+base16Mid = fromRight . C16.decode . C16.encode
+base64Mid = fromRight . C64.decode . C64.encode
+
+base64JSONMid :: C64.JsonByteString -> C64.JsonByteString
+base64JSONMid = fromJust . decode . encode
+
+fromRight :: Either a BS.ByteString -> BS.ByteString
+fromRight (Left _)  = error "failed decoding to ByteString"
+fromRight (Right b) = b
---
+++ test/Test/Serokell/Util/CommonSpec.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Test.Serokell.Util.CommonSpec
+       ( spec
+       ) where
+
+import           Data.Foldable         (toList)
+import           Data.List             (genericIndex, genericLength)
+import           Test.Hspec            (Spec, describe)
+import           Test.Hspec.QuickCheck (prop)
+import           Test.QuickCheck       (NonEmptyList (..))
+
+import qualified Serokell.Util.Common  as C
+
+spec :: Spec
+spec =
+    describe "Common" $ do
+{-        describe "enumerate" $
+            prop description_enumerateCheck enumerateCheckIndexes-}
+        describe "indexModulo" $
+            prop description_indexModulo indexModuloCorrectIndex
+  where
+{-    description_enumerateCheck = "enumerates a structure in sequence, " ++
+      "starting from index 0"-}
+    description_indexModulo = "returns the element of the the list with " ++
+      "given index modulo length of the list"
+
+enumerateCheckIndexes
+    :: (Traversable t, Show a, Show (t a))
+    => t a -> Bool
+enumerateCheckIndexes values =
+    let indexedVals = C.enumerate values
+        indexList   = toList $ fmap fst indexedVals
+        indexNum    = genericLength indexList
+    in indexList == [0 .. indexNum-1]
+
+indexModuloCorrectIndex
+    :: NonEmptyList Int -> Int -> Bool
+indexModuloCorrectIndex (getNonEmpty -> list) ind =
+    let len = genericLength list
+        atModuloIndex = list `genericIndex` (ind `mod` len)
+    in atModuloIndex == C.indexModulo list ind
+
+indexedSublistCheckIndexes
+    :: (Int, Int) -> [Int] -> Bool
+indexedSublistCheckIndexes (lo, hi) list
+    | hi <= lo = indexList == []
+    | otherwise = [lo .. hi - 1] == indexList
+
+  where
+    indexList = map fst $ C.indexedSubList (lo, hi) list
