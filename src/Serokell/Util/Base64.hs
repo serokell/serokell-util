946abea26f55b93d322f685bb208466135316d9b
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-09-17 03:49:56 +0300

    Fix typo

--- src/Serokell/Util/Base64.hs
+++ src/Serokell/Util/Base64.hs
@@ -19,11 +19,11 @@ import           Data.Either.Combinators    (mapLeft)
 import qualified Data.Text                  as T
 import           Data.Text.Encoding         (decodeUtf8, encodeUtf8)
 
--- | Apply base64url encoding to strict ByteString.
+-- | Apply base64 encoding to strict ByteString.
 encode :: BS.ByteString -> T.Text
 encode = decodeUtf8 . B64.encode
 
--- | Decode base64url-encoded ByteString.
+-- | Decode base64-encoded ByteString.
 decode :: T.Text -> Either T.Text BS.ByteString
 decode = mapLeft T.pack . B64.decode . encodeUtf8
 
