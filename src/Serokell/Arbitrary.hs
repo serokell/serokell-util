8dd05cd73d46f5f32f98f6544b6dd982da7a9fd7
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-09-24 17:57:19 +0100

    [SRK-22] Refactor tests and clean up VariantSpec

--- src/Serokell/Arbitrary.hs
+++ src/Serokell/Arbitrary.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell    #-}
 
 -- | Arbitrary instances for Serokell datatypes
-module Serokell.Arbitrary () where
+module Serokell.Arbitrary
+       ( VariantNoBytes (..)
+       , VariantOnlyBytes (..)
+       ) where
 
 import           Data.ByteString               as BS hiding (zip)
 import qualified Data.HashMap.Lazy             as H (fromList)
 import           Data.Vector                   (fromList)
-import           Test.QuickCheck               (Arbitrary (..), Gen,
-                                                choose, frequency, oneof, sized)
+import           GHC.Generics                  (Generic)
+import           Test.QuickCheck               (Arbitrary (..), Gen, choose,
+                                                genericShrink, frequency, oneof,
+                                                sized)
 import           Test.QuickCheck.Instances     ()
 
 import           Serokell.Data.Variant.Variant (Variant (..))
@@ -17,49 +23,68 @@ import qualified Serokell.Util.Base64          as S
 instance Arbitrary S.JsonByteString where
     arbitrary = S.JsonByteString <$> (arbitrary :: Gen BS.ByteString)
 
+newtype VariantNoBytes = NoBytes
+    { getVariant :: Variant
+    } deriving (Show, Eq, Generic)
+
+newtype VariantOnlyBytes = OnlyBytes
+    { getVarBytes :: Variant
+    } deriving (Show, Eq, Generic)
+
+instance Arbitrary VariantOnlyBytes where
+    arbitrary = OnlyBytes <$> oneof [VarBytes <$> arbitrary]
+    shrink = genericShrink
+
+instance Arbitrary VariantNoBytes where
+    arbitrary = NoBytes <$> (sized $ \n -> genVariant False (n*50 + 1))
+    shrink = genericShrink
+
 instance Arbitrary Variant where
-    arbitrary = sized $ \n -> genVariant (n*50 + 1)
+    arbitrary = sized $ \n -> genVariant True (n*50 + 1)
+    shrink = genericShrink
 
 -- | Generate something with at most N constructors. Say how many constructors
--- actually are in the generated 'Variant'.
-genVariant :: Int -> Gen Variant
-genVariant 1 = genFlatVariant
-genVariant n = do
+-- actually are in the generated 'Variant'. Because 'VarBytes' JSON decoding
+-- presents a problem, this generating function won't generate this constructor
+-- unless given a true boolean flag.
+genVariant :: Bool -> Int -> Gen Variant
+genVariant b 1 = genFlatVariant b
+genVariant b n = do
     frequency
         -- No reason for “3”, it just works well.
-        [ (3, genFlatVariant)
+        [ (3, genFlatVariant b)
         -- The more constructors we have to generate, the more likely we are
         -- to choose something nested instead of something flat.
         , (truncate (logBase 2 (fromIntegral n) :: Double),
-             oneof [genListVariant n, genMapVariant n])
+             oneof [genListVariant b n, genMapVariant b n])
         ]
 
-genFlatVariant :: Gen Variant
-genFlatVariant = oneof
+genFlatVariant :: Bool -> Gen Variant
+genFlatVariant bytes = oneof $
     [ pure VarNone
     , VarBool <$> arbitrary
     , VarInt <$> arbitrary
     , VarUInt <$> arbitrary
     , VarFloat <$> arbitrary
-    , VarBytes <$> arbitrary
     , VarString <$> arbitrary
-    ]
+    ] ++ if bytes then [VarBytes <$> arbitrary]
+                  else []
 
 -- | Generate a list of variants, using at most N constructors in total.
-genBoundedVariants :: Int -> Gen [Variant]
-genBoundedVariants 0 = return []
-genBoundedVariants n = do
+genBoundedVariants :: Bool -> Int -> Gen [Variant]
+genBoundedVariants _ 0 = return []
+genBoundedVariants b n = do
     v_cons <- choose (1, n)
-    (:) <$> genVariant v_cons
-        <*> genBoundedVariants (n-v_cons)
+    (:) <$> genVariant b v_cons
+        <*> genBoundedVariants b (n-v_cons)
 
-genListVariant :: Int -> Gen Variant
-genListVariant n = VarList . fromList <$> genBoundedVariants (n-1)
+genListVariant :: Bool -> Int -> Gen Variant
+genListVariant b n = VarList . fromList <$> genBoundedVariants b (n-1)
 
-genMapVariant :: Int -> Gen Variant
-genMapVariant n = do
-    keys <- genBoundedVariants ((n-1) `div` 2)
-    vals <- genBoundedVariants ((n-1) `div` 2)
+genMapVariant :: Bool -> Int -> Gen Variant
+genMapVariant b n = do
+    keys <- genBoundedVariants b ((n-1) `div` 2)
+    vals <- genBoundedVariants b ((n-1) `div` 2)
     -- Lengths of keys and vals may not match and so we would get less
     -- constructors due to truncation, but we don't care.
     return $ VarMap $ H.fromList $ zip keys vals
--- test/Test/Serokell/Data/Variant/VariantSpec.hs
+++ test/Test/Serokell/Data/Variant/VariantSpec.hs
@@ -1,20 +1,25 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns        #-}
 
 module Test.Serokell.Data.Variant.VariantSpec
        ( spec
        ) where
 
-import           Data.Aeson            (decode, encode)
+import qualified Data.Aeson            as A (decode, encode)
 import qualified Data.Binary           as B (decode, encode)
-import           Data.Maybe            (fromJust)
+import qualified Data.HashMap.Lazy     as HM (elems, fromList, keys)
 import           Data.MessagePack      (fromObject, toObject)
+import qualified Data.SafeCopy         as SC (safeGet, safePut)
 import           Data.Scientific       (floatingOrInteger, fromFloatDigits)
+import qualified Data.Serialize        as C (decode, encode, runGet, runPut)
+import           Data.Text             (unpack)
 import qualified Data.Vector           as V (map)
 import           Test.Hspec            (Spec, describe)
 import           Test.Hspec.QuickCheck (prop)
 import           Test.QuickCheck       ((===))
 
-import           Serokell.Arbitrary    ()
+import           Serokell.Arbitrary    (VariantNoBytes (..),
+                                        VariantOnlyBytes (..))
 import qualified Serokell.Data.Variant as S
 import qualified Serokell.Util.Base64  as S
 import           Serokell.Util.Text    (show')
@@ -22,63 +27,77 @@ import           Serokell.Util.Text    (show')
 spec :: Spec
 spec = describe "Variant" $ do
            describe "Identity Properties" $ do
-               {-prop "Serialization" $
-                   \(a :: S.Variant) -> variantMid a
+               describe "JSON" $ do
+                   prop "Variant (No VarBytes)" $
+                       \(getVariant -> a) -> (jsonFixer a) === jsonMid a
+                   prop "Variant (Only VarBytes)" $
+                       \(getVarBytes -> a) -> a === (bytesFun $ jsonMid a)
                prop "MessagePack" $
-                   \(a :: S.Variant) -> msgPkFun a === msgPackMid a-}
+                   \(a :: S.Variant) -> (msgPkFixer a) === msgPkMid a
                prop "Binary" $
                    \(a :: S.Variant) -> a === binMid a
-{-
-variantMid a =
-    let json = jsonFun a
-    in case a of
-          S.VarBytes _ -> a === bytesFun json
-          S.VarInt i -> if i < 0 then a === json
-                                 else (S.VarUInt $ fromIntegral i) === json
-          S.VarFloat _ -> json === floatFun a
-          S.VarMap m -> (S.VarMap $ fmap toStr m) === json
-          S.VarList l -> (S.VarList $ V.map keysToStr l) === json
-          _ -> a === json
+               prop "SafeCopy" $
+                   \(a :: S.Variant) -> a === safeCopyMid a
+               prop "Serialize" $
+                   \(a :: S.Variant) -> a === cerealMid a
 
-keysToStr (S.VarMap m)  = S.VarMap $ fmap toStr m
-keysToStr (S.VarList l) = S.VarList $ V.map keysToStr l
-keysToStr v             = v
-
-jsonFun :: S.Variant -> S.Variant
-jsonFun = fromJust . decode . encode
-
-bytesFun :: S.Variant -> S.Variant
-bytesFun (S.VarString s) = S.VarBytes right
-  where
-     right = fromRight $ S.decode s
-     fromRight (Right x) = x
-     fromRight _ = error "[bytesFun:] fromRight called with Left argument!"
-bytesFun _ = error "[bytesFun:] called with Variant that was not VarBytes"
-
-floatFun :: S.Variant -> S.Variant
-floatFun (S.VarFloat f) =
+jsonFixer :: S.Variant -> S.Variant
+jsonFixer (S.VarMap m) = let ks = map toStr $ HM.keys m
+                             vs = map jsonFixer $ HM.elems m
+                             m' = HM.fromList $ zip ks vs
+                         in S.VarMap m'
+jsonFixer (S.VarList l) = S.VarList $ V.map jsonFixer l
+jsonFixer v@(S.VarInt i) =
+    if i < 0 then v
+             else (S.VarUInt $ fromIntegral i)
+jsonFixer (S.VarFloat f) =
     case floatingOrInteger $ fromFloatDigits f of
         Left float -> S.VarFloat float
         Right int -> if int < 0 then S.VarInt int
                                 else S.VarUInt $ fromIntegral int
-floatFun _ = error "[floatFun:] called with Variant that was not VarFloat"
+jsonFixer v = v
 
 toStr :: S.Variant -> S.Variant
 toStr var = stringVar var
+  where
+    stringVar :: S.Variant -> S.Variant
+    stringVar = S.VarString . show'
 
-stringVar :: S.Variant -> S.Variant
-stringVar = S.VarString . show'
+jsonMid :: S.Variant -> S.Variant
+jsonMid = maybe err id . A.decode . A.encode
+   where
+     err = error "[VariantSpec] Failed JSON decoding"
 
-msgPackMid :: S.Variant -> S.Variant
-msgPackMid a =
-    case a of
-        S.VarInt i ->
-            if i < 0 then a
-                     else (S.VarUInt $ fromIntegral i)
-        _ -> a
+bytesFun :: S.Variant -> S.Variant
+bytesFun (S.VarString s) = S.VarBytes right
+  where
+     right = either (error . unpack) id $ S.decode s
+bytesFun _ = error "[bytesFun:] called with Variant that was not VarBytes"
 
-msgPkFun :: S.Variant -> S.Variant
-msgPkFun = fromJust . fromObject . toObject-}
+msgPkFixer :: S.Variant -> S.Variant
+msgPkFixer (S.VarMap m) = let ks = map msgPkFixer $ HM.keys m
+                              vs = map msgPkFixer $ HM.elems m
+                              m' = HM.fromList $ zip ks vs
+                          in S.VarMap m'
+msgPkFixer (S.VarList l) = S.VarList $ V.map msgPkFixer l
+msgPkFixer v@(S.VarInt i) =
+    if i < 0 then v
+             else (S.VarUInt $ fromIntegral i)
+msgPkFixer v@(S.VarUInt i) =
+    if i >= 0 then v
+             else (S.VarInt $ fromIntegral i)
+msgPkFixer v = v
+
+msgPkMid :: S.Variant -> S.Variant
+msgPkMid = maybe err id . fromObject . toObject
+  where
+    err = error "[VariantSpec] Failed MessagePack decoding"
 
 binMid :: S.Variant -> S.Variant
 binMid = B.decode . B.encode
+
+cerealMid :: S.Variant -> S.Variant
+cerealMid = either error id . C.decode . C.encode
+
+safeCopyMid :: S.Variant -> S.Variant
+safeCopyMid = either error id . C.runGet SC.safeGet . C.runPut . SC.safePut
