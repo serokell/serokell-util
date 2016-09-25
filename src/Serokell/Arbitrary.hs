e00700f7701d9070095a4f4afc90801cd576f4f3
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-09-25 18:56:50 +0100

    [SRK-22] Refactor Arbitrary Variant instances for JSON testing

--- src/Serokell/Arbitrary.hs
+++ src/Serokell/Arbitrary.hs
@@ -36,11 +36,11 @@ instance Arbitrary VariantOnlyBytes where
     shrink = genericShrink
 
 instance Arbitrary VariantNoBytes where
-    arbitrary = NoBytes <$> (sized $ \n -> genVariant False (n*50 + 1))
+    arbitrary = NoBytes <$> (sized $ \n -> genVariant (n*50 + 1))
     shrink = genericShrink
 
 instance Arbitrary Variant where
-    arbitrary = sized $ \n -> genVariant True (n*50 + 1)
+    arbitrary = sized $ \n -> genVariant (n*50 + 1)
     shrink = genericShrink
 
 -- | Generate something with at most N constructors. Say how many constructors
@@ -49,44 +49,43 @@ instance Arbitrary Variant where
 -- and it is difficult to tell whether it was decoded from a 'VarBytes' or
 -- another 'VarString' - this generating function won't generate this
 -- constructor unless given a true boolean flag.
-genVariant :: Bool -> Int -> Gen Variant
-genVariant b 1 = genFlatVariant b
-genVariant b n = do
+genVariant :: Int -> Gen Variant
+genVariant 1 = genFlatVariant
+genVariant n = do
     frequency
         -- No reason for “3”, it just works well.
-        [ (3, genFlatVariant b)
+        [ (3, genFlatVariant)
         -- The more constructors we have to generate, the more likely we are
         -- to choose something nested instead of something flat.
         , (truncate (logBase 2 (fromIntegral n) :: Double),
-             oneof [genListVariant b n, genMapVariant b n])
+             oneof [genListVariant n, genMapVariant n])
         ]
 
-genFlatVariant :: Bool -> Gen Variant
-genFlatVariant bytes = oneof $
+genFlatVariant :: Gen Variant
+genFlatVariant = oneof $
     [ pure VarNone
     , VarBool <$> arbitrary
     , VarInt <$> arbitrary
     , VarUInt <$> arbitrary
     , VarFloat <$> arbitrary
     , VarString <$> arbitrary
-    ] ++ if bytes then [VarBytes <$> arbitrary]
-                  else []
+    ]
 
 -- | Generate a list of variants, using at most N constructors in total.
-genBoundedVariants :: Bool -> Int -> Gen [Variant]
-genBoundedVariants _ 0 = return []
-genBoundedVariants b n = do
+genBoundedVariants :: Int -> Gen [Variant]
+genBoundedVariants 0 = return []
+genBoundedVariants n = do
     v_cons <- choose (1, n)
-    (:) <$> genVariant b v_cons
-        <*> genBoundedVariants b (n-v_cons)
+    (:) <$> genVariant v_cons
+        <*> genBoundedVariants (n-v_cons)
 
-genListVariant :: Bool -> Int -> Gen Variant
-genListVariant b n = VarList . fromList <$> genBoundedVariants b (n-1)
+genListVariant :: Int -> Gen Variant
+genListVariant n = VarList . fromList <$> genBoundedVariants (n-1)
 
-genMapVariant :: Bool -> Int -> Gen Variant
-genMapVariant b n = do
-    keys <- genBoundedVariants b ((n-1) `div` 2)
-    vals <- genBoundedVariants b ((n-1) `div` 2)
+genMapVariant :: Int -> Gen Variant
+genMapVariant n = do
+    keys <- genBoundedVariants ((n-1) `div` 2)
+    vals <- genBoundedVariants ((n-1) `div` 2)
     -- Lengths of keys and vals may not match and so we would get less
     -- constructors due to truncation, but we don't care.
     return $ VarMap $ H.fromList $ zip keys vals
