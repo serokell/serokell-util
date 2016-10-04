6e92cdb7476553e415a0f7e60cd0f5dfb77605a2
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-04 17:41:23 +0300

    Change verifyGeneric type

--- src/Serokell/Util/Verify.hs
+++ src/Serokell/Util/Verify.hs
@@ -1,23 +1,26 @@
 -- | General-purpose utility functions
 
 module Serokell.Util.Verify
-       ( verifyGeneric
+       ( VerificationRes (..)
+       , verifyGeneric
        ) where
 
-import           Serokell.Util.Exceptions (throwText)
+import qualified Data.Text as T
 
-import           Control.Monad.Catch      (MonadThrow)
-import qualified Data.Text                as T
+data VerificationRes
+    = Verified
+    | NotVerified ![T.Text]
+    deriving (Show)
 
--- | This function takes list of (predicate, message) pairs and checks each predicate.
--- If predicate is False it's considered an error.
--- If there is at least one error this function throws error message, otherwise () is returned
--- It's useful to verify some data before using it.
+-- | This function takes list of (predicate, message) pairs and checks
+-- each predicate.  If predicate is False it's considered an error.
+-- If there is at least one error this function returns NotVerified,
+-- otherwise Verified is returned.  It's useful to verify some data
+-- before using it.
 -- Example usage: `verifyGeneric [(checkA, "A is bad"), (checkB, "B is bad")]`
-verifyGeneric :: MonadThrow m
-              => [(Bool, T.Text)] -> m ()
+verifyGeneric :: [(Bool, T.Text)] -> VerificationRes
 verifyGeneric errors
-  | not $ null messages = throwText $ T.intercalate " " messages
-  | otherwise = return ()
+    | null messages = Verified
+    | otherwise = NotVerified messages
   where
     messages = map snd . filter (not . fst) $ errors
