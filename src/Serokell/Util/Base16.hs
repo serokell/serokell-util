a2711a163577165c6a15895239d88c6a53dd6bd2
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-04 21:29:29 +0300

    Add formatters for base16/base64

--- src/Serokell/Util/Base16.hs
+++ src/Serokell/Util/Base16.hs
@@ -3,13 +3,16 @@
 module Serokell.Util.Base16
        ( encode
        , decode
+       , formatBase16
+       , base16F
        ) where
 
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.Text              as T
 import qualified Data.Text.Encoding     as TE
-import           Formatting             (sformat, stext, (%))
+import           Data.Text.Lazy.Builder (Builder, fromText)
+import           Formatting             (Format, later, sformat, stext, (%))
 
 -- | Apply base16 encoding to strict ByteString.
 encode :: BS.ByteString -> T.Text
@@ -26,3 +29,11 @@ decode = handleError . B16.decode . TE.encodeUtf8
           sformat
               ("suffix is not in base-16 format: " % stext)
               (TE.decodeUtf8 rest)
+
+-- | Construct Builder from bytestring formatting it in Base16.
+formatBase16 :: BS.ByteString -> Builder
+formatBase16 = fromText . encode
+
+-- | Format which uses Base16 to print bytestring.
+base16F :: Format r (BS.ByteString -> r)
+base16F = later formatBase16
--- src/Serokell/Util/Base64.hs
+++ src/Serokell/Util/Base64.hs
@@ -5,19 +5,22 @@ module Serokell.Util.Base64
        , decode
        , encodeUrl
        , decodeUrl
+       , formatBase64
+       , base64F
        , JsonByteString (..)
        , JsonByteStringDeprecated (..)
        ) where
 
 import           Control.Monad              ((>=>))
-import           Data.Aeson                 (FromJSON (parseJSON),
-                                             ToJSON (toJSON))
+import           Data.Aeson                 (FromJSON (parseJSON), ToJSON (toJSON))
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Base64     as B64
 import qualified Data.ByteString.Base64.URL as B64url
 import           Data.Either.Combinators    (mapLeft)
 import qualified Data.Text                  as T
 import           Data.Text.Encoding         (decodeUtf8, encodeUtf8)
+import           Data.Text.Lazy.Builder     (Builder, fromText)
+import           Formatting                 (Format, later)
 
 -- | Apply base64 encoding to strict ByteString.
 encode :: BS.ByteString -> T.Text
@@ -34,6 +37,14 @@ encodeUrl = decodeUtf8 . B64url.encode
 -- | Decode base64url-encoded ByteString.
 decodeUrl :: T.Text -> Either T.Text BS.ByteString
 decodeUrl = mapLeft T.pack . B64url.decode . encodeUtf8
+
+-- | Construct Builder from bytestring formatting it in Base64.
+formatBase64 :: BS.ByteString -> Builder
+formatBase64 = fromText . encode
+
+-- | Format which uses Base64 to print bytestring.
+base64F :: Format r (BS.ByteString -> r)
+base64F = later formatBase64
 
 -- | Wrapper on top of ByteString with JSON serialization (in base64
 -- encoding).
