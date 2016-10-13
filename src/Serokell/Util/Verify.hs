4109abf141d427a4b741e42947dd3ab14a3ce5b4
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-13 18:45:07 +0300

    Util.Verify: improve laziness, rename VerificationRes c-tors, add helpers

--- src/Serokell/Util/Verify.hs
+++ src/Serokell/Util/Verify.hs
@@ -2,31 +2,46 @@
 
 module Serokell.Util.Verify
        ( VerificationRes (..)
+       , isVerFailure
+       , isVerSuccess
+
        , verifyGeneric
        ) where
 
 import qualified Data.Text as T
 
 data VerificationRes
-    = Verified
-    | NotVerified ![T.Text]
+    = VerSuccess
+    | VerFailure ![T.Text]
     deriving (Show)
 
+isVerSuccess :: VerificationRes -> Bool
+isVerSuccess VerSuccess = True
+isVerSuccess _          = False
+
+isVerFailure :: VerificationRes -> Bool
+isVerFailure (VerFailure _) = True
+isVerFailure _              = False
+
 instance Monoid VerificationRes where
-    mempty = Verified
-    mappend Verified a                        = a
-    mappend a Verified                        = a
-    mappend (NotVerified xs) (NotVerified ys) = NotVerified $ xs ++ ys
+    mempty = VerSuccess
+    mappend VerSuccess a = a
+    mappend (VerFailure xs) a =
+        VerFailure $
+        xs ++
+        case a of
+            VerSuccess    -> []
+            VerFailure ys -> ys
 
 -- | This function takes list of (predicate, message) pairs and checks
 -- each predicate.  If predicate is False it's considered an error.
--- If there is at least one error this function returns NotVerified,
--- otherwise Verified is returned.  It's useful to verify some data
+-- If there is at least one error this function returns VerFailure,
+-- otherwise VerSuccess is returned.  It's useful to verify some data
 -- before using it.
 -- Example usage: `verifyGeneric [(checkA, "A is bad"), (checkB, "B is bad")]`
 verifyGeneric :: [(Bool, T.Text)] -> VerificationRes
 verifyGeneric errors
-    | null messages = Verified
-    | otherwise = NotVerified messages
+    | null messages = VerSuccess
+    | otherwise = VerFailure messages
   where
     messages = map snd . filter (not . fst) $ errors
