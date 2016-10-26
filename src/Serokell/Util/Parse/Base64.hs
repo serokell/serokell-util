982616f4400f69d11a011b2fcda477a448f88f1b
George Agapov <george.agapov@gmail.com>
2016-10-26 16:43:00 +0300

    Add parsing helpers (for CLI parsing)

--- src/Serokell/Util/Parse/Base64.hs
+++ src/Serokell/Util/Parse/Base64.hs
@@ -4,26 +4,24 @@ module Serokell.Util.Parse.Base64
        ( base64
        , base64Url
        ) where
-import           Data.String                        (IsString (fromString))
+import qualified Data.ByteString                    as BS
 import           Data.Text                          (pack, unpack)
 import           Serokell.Util.Base64               (decode, decodeUrl)
-import           Serokell.Util.Parse.Common         (Parser, asciiAlphaNum, byte,
-                                                     countMinMax, limitedInt)
-import           Text.Parsec                        (many, many1, satisfy, try, (<?>),
-                                                     (<|>))
-import           Text.ParserCombinators.Parsec.Char (char, string)
+import           Serokell.Util.Parse.Common         (Parser, asciiAlphaNum)
+import           Text.Parsec                        (many, many1, (<|>))
+import           Text.ParserCombinators.Parsec.Char (char)
 
-base64 :: IsString s => Parser s
+base64 :: Parser BS.ByteString
 base64 = do
   str <- (++) <$> many1 (asciiAlphaNum <|> char '+' <|> char '/') <*> many (char '=')
   case decode $ pack str of
-    Left e  -> fail $ unpack e
-    Right _ -> return $ fromString str
+    Left e   -> fail $ unpack e
+    Right bs -> return bs
 
-base64Url :: IsString s => Parser s
+base64Url :: Parser BS.ByteString
 base64Url = do
   str <- (++) <$> many1 (asciiAlphaNum <|> char '_' <|> char '-') <*> many (char '=')
   case decodeUrl $ pack str of
-    Left e  -> fail $ unpack e
-    Right _ -> return $ fromString str
+    Left e   -> fail $ unpack e
+    Right bs -> return bs
 
