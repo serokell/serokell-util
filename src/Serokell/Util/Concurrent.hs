082e5ac1c11fbe5e565b7857103421e53b91daf2
Martoon <Martoon_00@mail.ru>
2016-11-30 16:26:49 +0300

    Add modifyTVarS

--- src/Serokell/Util/Concurrent.hs
+++ src/Serokell/Util/Concurrent.hs
@@ -2,13 +2,28 @@
 
 module Serokell.Util.Concurrent
        ( threadDelay
+       , modifyTVarS
        ) where
 
-import qualified Control.Concurrent  as Concurrent
-import           Control.Monad.Trans (MonadIO (liftIO))
-import           Data.Time.Units     (TimeUnit (toMicroseconds))
+import qualified Control.Concurrent          as Concurrent
+import           Control.Concurrent.STM      (STM)
+import           Control.Concurrent.STM.TVar (TVar, readTVar, writeTVar)
+import           Control.Monad.State         (StateT, runStateT)
+import           Control.Monad.Trans         (MonadIO (liftIO))
+import           Data.Time.Units             (TimeUnit (toMicroseconds))
+
 
 -- | Convenient version of Control.Concurrent.threadDelay which takes
 -- any time-unit and operates in any MonadIO
 threadDelay :: (MonadIO m, TimeUnit unit) => unit -> m ()
 threadDelay = liftIO . Concurrent.threadDelay . fromIntegral . toMicroseconds
+
+-- | Atomically modifies given `TVar`, associating state of given `StateT` with
+-- `TVar` entry.
+-- TODO: maybe generalize to any container and monad? Define as operator?
+modifyTVarS :: TVar s -> StateT s STM a -> STM a
+modifyTVarS t st = do
+    s <- readTVar t
+    (a, s') <- runStateT st s
+    writeTVar t s'
+    return a
