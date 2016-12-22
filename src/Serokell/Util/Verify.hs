fad519f0b2ecacedd4737efb86281c6d04abb688
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-22 21:42:08 +0300

    [SU-5] Add formatters for VerificationRes

--- src/Serokell/Util/Verify.hs
+++ src/Serokell/Util/Verify.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+
 -- | General-purpose utility functions
 
 module Serokell.Util.Verify
@@ -5,12 +7,23 @@ module Serokell.Util.Verify
        , isVerFailure
        , isVerSuccess
 
+       -- * Prety printing
+       , buildVerResFull
+       , buildVerResSingle
+       , verResFullF
+       , verResSingleF
+
        , verifyGeneric
        ) where
 
-import           Data.Semigroup (Semigroup)
-import qualified Data.Semigroup as Semigroup
-import qualified Data.Text      as T
+import           Data.Semigroup         (Semigroup)
+import qualified Data.Semigroup         as Semigroup
+import           Data.Text              (Text)
+import qualified Data.Text              as T
+import qualified Data.Text.Lazy.Builder as B
+import           Formatting             (Format, later)
+
+import           Serokell.Util.Text     (listBuilder)
 
 data VerificationRes
     = VerSuccess
@@ -50,3 +63,33 @@ verifyGeneric errors
     | otherwise = VerFailure messages
   where
     messages = map snd . filter (not . fst) $ errors
+
+----------------------------------------------------------------------------
+-- Pretty printing
+----------------------------------------------------------------------------
+
+-- | Format VerificationRes in a pretty way using all errors messages
+-- for VerFailure.
+buildVerResFull :: VerificationRes -> B.Builder
+buildVerResFull VerSuccess          = buildVerResImpl Nothing
+buildVerResFull (VerFailure errors) = buildVerResImpl $ Just errors
+
+-- | Format VerificationRes in a pretty way using only first message
+-- for VerFailure.
+buildVerResSingle :: VerificationRes -> B.Builder
+buildVerResSingle VerSuccess          = buildVerResImpl Nothing
+-- [SU-1] Use NonEmpty instead of unsafe head.
+buildVerResSingle (VerFailure errors) = buildVerResImpl $ Just $ [head errors]
+
+buildVerResImpl :: Maybe [T.Text] -> B.Builder
+buildVerResImpl Nothing       = "success"
+buildVerResImpl (Just errors) =
+    "failure: " `mappend` listBuilder @Text @Text @Text "[" "; " "]" errors
+
+-- | Formatter based on buildVerResFull.
+verResFullF :: Format r (VerificationRes -> r)
+verResFullF = later buildVerResFull
+
+-- | Formatter based on buildVerResSingle.
+verResSingleF :: Format r (VerificationRes -> r)
+verResSingleF = later buildVerResSingle
