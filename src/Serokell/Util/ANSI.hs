c72b6d7df3e820c9a95f62064328c923a9c6a732
Artyom <yom@artyom.me>
2017-02-25 17:17:37 +0300

    Switch Serokell.Util.ANSI to Universum
    as requested by @shersh

--- src/Serokell/Util/ANSI.hs
+++ src/Serokell/Util/ANSI.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- | Functions for working with ANSI-colored text.
 module Serokell.Util.ANSI
        ( Color(..)
@@ -5,27 +7,23 @@ module Serokell.Util.ANSI
        , withColoredMessages
        ) where
 
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Data.Monoid            (mappend)
-import           Data.Text              (Text)
-import qualified Data.Text              as T
-import qualified Data.Text.IO           as T
-import           System.Console.ANSI    (Color (..), ColorIntensity (Vivid),
-                                         ConsoleLayer (Foreground), SGR (Reset, SetColor),
-                                         setSGRCode)
+import           System.Console.ANSI (Color (..), ColorIntensity (Vivid),
+                                      ConsoleLayer (Foreground), SGR (Reset, SetColor),
+                                      setSGRCode)
+import           Universum
 
 -- | Prettify 'Text' message with 'Vivid' color.
 colorize :: Color -> Text -> Text
 colorize color msg =
-    T.pack (setSGRCode [SetColor Foreground Vivid color]) `mappend`
-    msg `mappend`
-    T.pack (setSGRCode [Reset])
+    toText (setSGRCode [SetColor Foreground Vivid color]) <>
+    msg <>
+    toText (setSGRCode [Reset])
 
 -- | Write colored message, do some action, write colored message.
 -- Intended for debug only.
 withColoredMessages :: MonadIO m => Color -> Text -> m a -> m a
 withColoredMessages color activity action = do
-    liftIO $ T.putStrLn (colorize color ("Entered " `mappend` activity))
+    putStrLn (colorize color ("Entered " <> activity))
     res <- action
-    liftIO $ T.putStrLn (colorize color ("Finished " `mappend` activity))
+    putStrLn (colorize color ("Finished " <> activity))
     return res
