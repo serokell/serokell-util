7c43f1f0d117599d94f64acec5101deea2f844a3
Ante Kegalj <akegalj@gmail.com>
2017-11-20 21:25:12 +0100

    Rename function listMapTuple -> listMap

--- src/Serokell/Util/Text.hs
+++ src/Serokell/Util/Text.hs
@@ -19,7 +19,7 @@ module Serokell.Util.Text
        , pairF
        , tripleF
        , listJson
-       , listMapTuple
+       , listMap
        , listJsonIndent
        , listCsv
        , mapJson
@@ -106,8 +106,8 @@ tripleF = later tripleBuilder
 listJson :: (Foldable t, Buildable a) => Format r (t a -> r)
 listJson = later listBuilderJSON
 
-listMapTuple :: (Traversable t, Buildable a, Buildable b) => Format r (t (a, b) -> r)
-listMapTuple = later mapBuilder
+listMap :: (Traversable t, Buildable a, Buildable b) => Format r (t (a, b) -> r)
+listMap = later mapBuilder
 
 listJsonIndent :: (Foldable t, Buildable a) => Word -> Format r (t a -> r)
 listJsonIndent = later . listBuilderJSONIndent
