93ef276e73361090a2df3625b51fcd3c30c5c0b5
vrom911 <vrom911@gmail.com>
2018-01-18 04:19:01 +0300

    [#22] Use o'clock, remove time funcs

--- src/Serokell/Util/Bench.hs
+++ src/Serokell/Util/Bench.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE ExplicitForAll      #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- | Benchmark related utils.
 
 module Serokell.Util.Bench
+#if ( __GLASGOW_HASKELL__ < 802 )
+       ( ) where
+#else
        ( getWallTime
        , getCpuTime
        , ElapsedTime (..)
@@ -9,26 +16,22 @@ module Serokell.Util.Bench
        , perSecond
        ) where
 
-import           Control.Monad.Trans (MonadIO (liftIO))
-import           Data.Text.Buildable (Buildable (build))
-import           Data.Time.Units     (Nanosecond, TimeUnit, convertUnit)
-import           Formatting          (bprint, shown, (%))
-import           System.Clock        (Clock (..), TimeSpec, diffTimeSpec,
-                                      getTime, toNanoSecs)
+import Universum
+
+import Fmt (Buildable (build), (+||), (||+))
+import System.Clock (Clock (..), TimeSpec, diffTimeSpec, getTime, toNanoSecs)
+import Time (KnownRat, Time, ns, toUnit)
 
 -- | Get current wall-clock time as any time unit.
-getWallTime :: (MonadIO m, TimeUnit a) => m a
+getWallTime :: forall unit m . (MonadIO m, KnownRat unit) => m (Time unit)
 getWallTime = timeSpecToUnit <$> getTime' Realtime
 
 -- | Get current CPU time as any time unit.
-getCpuTime :: (MonadIO m, TimeUnit a) => m a
+getCpuTime :: forall unit m . (MonadIO m, KnownRat unit) => m (Time unit)
 getCpuTime = timeSpecToUnit <$> getTime' ProcessCPUTime
 
-timeSpecToUnit
-    :: TimeUnit a
-    => TimeSpec -> a
-timeSpecToUnit =
-    convertUnit . (fromIntegral :: Integer -> Nanosecond) . toNanoSecs
+timeSpecToUnit :: forall unit . KnownRat unit => TimeSpec -> Time unit
+timeSpecToUnit = toUnit @unit . ns . fromIntegral . toNanoSecs
 
 -- | Data type describing time passed during execution of something.
 data ElapsedTime = ElapsedTime
@@ -38,10 +41,7 @@ data ElapsedTime = ElapsedTime
 
 instance Buildable ElapsedTime where
     build ElapsedTime{..} =
-        bprint
-            ("(CPU time = " % shown % ", wall time = " % shown % ")")
-            elapsedCpuTime
-            elapsedWallTime
+        "(CPU time = "+||elapsedCpuTime||+", wall time = "+||elapsedWallTime||+")"
 
 getTime' :: MonadIO m => Clock -> m TimeSpec
 getTime' = liftIO . getTime
@@ -49,14 +49,14 @@ getTime' = liftIO . getTime
 -- | Run given action and measure how much time it took.
 measureTime :: MonadIO m => m a -> m (ElapsedTime, a)
 measureTime action = do
-    cpuTimeBefore <- getTime' ProcessCPUTime
+    cpuTimeBefore  <- getTime' ProcessCPUTime
     wallTimeBefore <- getTime' Realtime
-    res <- action
-    wallTimeAfter <- getTime' Realtime
-    cpuTimeAfter <- getTime' ProcessCPUTime
+    res            <- action
+    wallTimeAfter  <- getTime' Realtime
+    cpuTimeAfter   <- getTime' ProcessCPUTime
     return
         ( ElapsedTime
-          { elapsedCpuTime = cpuTimeAfter `diffTimeSpec` cpuTimeBefore
+          { elapsedCpuTime  = cpuTimeAfter  `diffTimeSpec` cpuTimeBefore
           , elapsedWallTime = wallTimeAfter `diffTimeSpec` wallTimeBefore
           }
         , res)
@@ -72,3 +72,4 @@ perSecond :: (Real a, Fractional b) => a -> TimeSpec -> b
 perSecond n time =
     fromRational $
     toRational n / (fromIntegral (max 1 $ toNanoSecs time) * 1.0e9)
+#endif
