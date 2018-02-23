2323c42a3e5ee565745c16d7da2b5024fe4d6b67
DanielRrr <chelsea2005joecole@icloud.com>
2018-02-23 15:59:39 +0300

    erased some rubbish from OptParse.hs #34

--- src/Serokell/Util/OptParse.hs
+++ src/Serokell/Util/OptParse.hs
@@ -4,29 +4,18 @@
 
 module Serokell.Util.OptParse
        ( fromStr
-       , strArgument
-       , strOption
        , fromParsec
        ) where
 
 import Universum
 
 import Data.String (IsString (fromString))
-import Options.Applicative (ArgumentFields, Mod, OptionFields, Parser, ReadM, argument,
-                            eitherReader, option, str)
+import Options.Applicative (ReadM, eitherReader, str)
 import Text.Parsec (Parsec, parse)
 
 -- | Reader which uses IsString instance for parsing
 fromStr :: IsString s => ReadM s
 fromStr = fromString <$> str
-
--- | Parse argument using IsString instance
-strArgument :: IsString s => Mod ArgumentFields s -> Parser s
-strArgument = argument fromStr
-
--- | Parse option using IsString instance
-strOption :: IsString s => Mod OptionFields s -> Parser s
-strOption = option fromStr
 
 fromParsec :: Parsec Text () a -> ReadM a
 fromParsec parser =
